ecb7272fb2a374937fcf091b5b7d7f54379741e9
shuta13 <hollystarsun@gmail.com>
2021-05-14 17:57:36 +0900

    update: remove moment

--- server/src/routes/static.js
+++ server/src/routes/static.js
@@ -6,8 +6,6 @@ import { PUBLIC_PATH, CLIENT_DIST_PATH, UPLOAD_PATH } from '../paths';
 
 const router = Router();
 
-console.log(PUBLIC_PATH);
-
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 router.use(history());
 
