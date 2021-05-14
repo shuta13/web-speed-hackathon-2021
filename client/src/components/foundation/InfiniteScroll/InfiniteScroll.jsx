b9021c72c45f8d1d4fa9f037a7093707b85fcd1b
shuta13 <hollystarsun@gmail.com>
2021-05-14 09:53:09 +0900

    fix: infinite scroll

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -13,9 +13,11 @@ const InfiniteScroll = ({ children, fetchMore }) => {
   React.useEffect(() => {
     const handler = () => {
       // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
-        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      }).every(Boolean);
+      // const hasReached = Array.from(Array(2 ** 18), () => {
+      //   return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
+      // }).every(Boolean);
+
+      const hasReached = window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
 
       // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
       if (hasReached && !prevReachedRef.current) {
@@ -25,14 +27,14 @@ const InfiniteScroll = ({ children, fetchMore }) => {
       prevReachedRef.current = hasReached;
     };
 
-    document.addEventListener('wheel', handler, { passive: false });
-    document.addEventListener('touchmove', handler, { passive: false });
-    document.addEventListener('resize', handler, { passive: false });
+    // document.addEventListener('wheel', handler, { passive: false });
+    // document.addEventListener('touchmove', handler, { passive: false });
+    // document.addEventListener('resize', handler, { passive: false });
     document.addEventListener('scroll', handler, { passive: false });
     return () => {
-      document.removeEventListener('wheel', handler);
-      document.removeEventListener('touchmove', handler);
-      document.removeEventListener('resize', handler);
+      // document.removeEventListener('wheel', handler);
+      // document.removeEventListener('touchmove', handler);
+      // document.removeEventListener('resize', handler);
       document.removeEventListener('scroll', handler);
     };
   }, [fetchMore]);
